1d3bd17b0a21287065839ba057ef7ffac282db82
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-03 22:25:12 +0300

    added player buttons in js

---
+++ main.js
@@ -0,0 +1,5 @@
+const playerROundPints=document.querySelector('.player-score');
+const fire=document.querySelector('.p-fire');
+const water=document.querySelector(".p-water");
+const earth=document.querySelector(".p-earth")
+
