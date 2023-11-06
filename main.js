9e5978a9c251760e19fcfabf48445a0f5038f931
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-06 13:31:26 +0300

    added the computer guess function

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-const playerROundPints=document.querySelector('.player-score');
-const fire=document.querySelector('.p-fire');
-const water=document.querySelector(".p-water");
-const earth=document.querySelector(".p-earth")
+const playerROundPints = document.querySelector(".player-score");
+const fire = document.querySelector(".p-fire");
+const water = document.querySelector(".p-water");
+const earth = document.querySelector(".p-earth");
+
+function getComputerChoice() {
+	let random = Math.floor(Math.random() * 3);
+	let choice;
+	switch (random) {
+		case 0:
+			choice = "FIRE";
+			break;
+
+		case 1:
+			choice = "WATER";
+			break;
+		case 2:
+			choice = "EARTH";
+			break;
+		default:
+			break;
+	}
+  return choice;
+}
+
 
